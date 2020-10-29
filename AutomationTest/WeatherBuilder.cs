7756cfebdb13f333272dca52cccdb216761c1bd5
Joao Paulo Dias <joao.dias@rockcontent.com>
2020-10-29 16:08:17 -0300

    test: create WeatherBuilder

---
+++ AutomationTest/WeatherBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationIntegrationTest
+{
+    public class WeatherBuilder
+    {
+        private Weather _weather;
+
+        public static WeatherBuilder New()
+        {
+            return new WeatherBuilder();
+        }
+
+        private WeatherBuilder()
+        {
+            _weather = new Weather();
+            _weather.City = "Boston";
+            _weather.State = "MA";
+        }
+
+        public WeatherBuilder WithCity(string city)
+        {
+            _weather.City = city;
+            return this;
+        }
+
+        public WeatherBuilder WithState(string state)
+        {
+            _weather.State = state;
+            return this;
+        }
+
+        public Weather Build()
+        {
+            return _weather;
+        }
+
+    }
+}
