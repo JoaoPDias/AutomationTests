7150ee14dbbbc63f92bfe7a47f83152040a59260
Joao Paulo Dias <joao.dias@rockcontent.com>
2020-10-29 16:05:51 -0300

    test: create C# Project

---
+++ AutomationTest/Weather.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace AutomationIntegrationTest
+{
+    public class Weather
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+        public string TempF { get; set; }
+        public string TempC { get; set; }
+        [JsonProperty("Weather")]
+        public string WeatherDescription { get; set; }
+    }
+}
