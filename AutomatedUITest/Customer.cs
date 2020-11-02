421dbcf53aa5a8d804ffecf736cc20496de8d8eb
Joao Paulo Dias <joao.dias@rockcontent.com>
2020-11-02 12:49:00 -0300

    Create Builder To Customer Class

---
+++ AutomatedUITest/Customer.cs
@@ -0,0 +1,12 @@
+namespace AutomatedUITest
+{
+    public class Customer
+    {
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ AutomatedUITest/CustomerBuilder.cs
@@ -0,0 +1,58 @@
+using Bogus;
+
+namespace AutomatedUITest
+{
+    public class CustomerBuilder
+    {
+        private readonly Customer _customer;
+
+        private CustomerBuilder()
+        {
+            _customer = new Customer();
+            Faker faker = new Faker();
+            _customer.Address = faker.Address.StreetAddress();
+            _customer.Name = faker.Name.FirstName() + faker.Name.LastName();
+            _customer.Phone = faker.Phone.PhoneNumber("+###-##-###-####");
+            _customer.Email = faker.Person.Email;
+            _customer.PostalCode = faker.Address.ZipCode("#####-###");
+        }
+
+        public static CustomerBuilder New()
+        {
+            return new CustomerBuilder();
+        }
+
+        public CustomerBuilder WithAddress(string address)
+        {
+            _customer.Address = address;
+            return this;
+        }
+
+        public CustomerBuilder WithName(string name)
+        {
+            _customer.Name = name;
+            return this;
+        }
+
+        public CustomerBuilder WithPhone(string phone)
+        {
+            _customer.Phone = phone;
+            return this;
+        }
+
+        public CustomerBuilder WithPostalCode(string postalCode)
+        {
+            _customer.PostalCode = postalCode;
+            return this;
+        }
+        public CustomerBuilder WithEmail(string email)
+        {
+            _customer.Email = email;
+            return this;
+        }
+        public Customer Build()
+        {
+            return _customer;
+        }
+    }
+}
